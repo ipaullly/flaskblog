74e877324a39e4501c3c0147485f5dd6a1f06963
ipaully <ipaully22@gmail.com>
2018-10-05 00:50:49 +0300

    Add render template functionality

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'John doe',
+        'title': 'blog post dres',
+        'content': 'Second post content',
+        'date_posted': 'April 32, 2018'
+    },
+    {
+        'author': 'Gory Eshlat',
+        'title': 'blog post fux',
+        'content': 'the third reich post content',
+        'date_posted': 'June 12, 2018'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About page</h1>"
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
