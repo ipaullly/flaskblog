0d7ca33504530b2772d49dce2e478f22f54a6135
ipaully <ipaully22@gmail.com>
2018-10-10 06:53:31 +0300

    Add custom email & username validators

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 class RegistrationFrom(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
@@ -9,6 +10,14 @@ class RegistrationFrom(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
     
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please select another one to register account')
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please select another one to register account')
 class LoginFrom(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email(),])
     password = PasswordField('Password', validators=[DataRequired(),])
